8b37a363f77b3674dba15d2a381e0f0c62b428b9
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2024-06-23 12:37:29 -0300

    feat: add type to get users route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserOutput } from './dto/user-output.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getUser(): Promise<any> {
+  async getUser(): Promise<UserOutput[]> {
     return this.userService.getUser();
   }
 }
