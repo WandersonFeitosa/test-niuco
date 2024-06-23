3bd30cdb34a97645e5ea653c37403350801842cb
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2024-06-23 12:47:01 -0300

    docs: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(port)
+
+  const config = new DocumentBuilder()
+    .setTitle('Niuco Challenge API')
+    .setDescription('Niuco Challenge API')
+    .setVersion('1.0')
+    .addTag('Niuco Challenge API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserOutput } from './dto/user-output.dto';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOkResponse({
+    description: 'List of users',
+    isArray: true,
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          name: { type: 'string' },
+          email: { type: 'string' },
+          last_activity: { type: 'string' },
+          status: { type: 'boolean' },
+          premium: { type: 'boolean' },
+        },
+      },
+    },
+  })
   async getUser(): Promise<UserOutput[]> {
     return this.userService.getUser();
   }
